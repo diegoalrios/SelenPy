a9ff372aca7a92a4292aed5ef1a9a2a518296787
Hindre <diego.al.rios96@gmail.com>
2020-10-01 22:45:42 -0500

    prueba tecnica

--- mercadolibre.py
+++ mercadolibre.py
@@ -15,7 +15,6 @@ class Typos(unittest.TestCase):
             seach_input=drv.find_element_by_xpath('//input[@class="nav-search-input"]')
             seach_input.send_keys('amd')
             seach_input.submit()
-
         else:
             self.assertEqual(page_tittle,drv.title)
     def tearDown(self):
